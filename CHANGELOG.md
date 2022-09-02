865db9567693e60dbbd1212dd6fdb51380120ecc
Tom <tomvogel0@gmail.com>
2022-09-02 14:16:09 +0200

    Adding changelog description

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
 ## 0.0.1
 
-* TODO: Describe initial release.
+A package to have a nice cursor.
+
+# How to use
+To use the package simply wrap a widget of your page or the whole screen with the BubbleCursor widget. Then add some customization and you are ready to go!
+
+Code will come soon!
--- lib/bubble_cursor.dart
+++ lib/bubble_cursor.dart
@@ -6,9 +6,15 @@ import 'package:flutter/material.dart';
 
 class BubbleCursor extends StatefulWidget {
   final Widget? widget;
-  final double? size;/// Normal 150
-  final Color? color;/// Normal white
-  final int? speed;/// Normal 2
+  final double? size;
+
+  /// Normal 150
+  final Color? color;
+
+  /// Normal white
+  final int? speed;
+
+  /// Normal 2
 
   const BubbleCursor({
     super.key,
@@ -27,57 +33,55 @@ class _BubbleCursorState extends State<BubbleCursor> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: MouseRegion(
-        onEnter: (details) {
-          setState(() {
-            mousePosition = details.position;
-          });
-        },
-        onHover: (details) {
-          setState(() {
-            mousePosition = details.position;
-          });
-        },
-        onExit: (details) {
-          setState(() {
-            mousePosition = details.position;
-          });
-        },
-        child: Stack(
-          children: [
-            if (widget.widget != null) widget.widget!,
-            if (mousePosition != Offset.zero)
-              AnimatedPositioned(
-                duration: Duration(milliseconds: (widget.speed ?? 2) * 100),
-                left: mousePosition.dx - (widget.size ?? 150 / 2),
-                top: mousePosition.dy - (widget.size ?? 150 / 2),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(180),
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 3.5, sigmaY: 3.5),
-                    child: Container(
-                      width: widget.size ?? 150,
-                      height: widget.size ?? 150,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(180),
-                        border: Border.all(
-                          width: 3,
-                          color: Colors.grey,
-                        ),
-                        gradient: RadialGradient(
-                          colors: [
-                            Colors.transparent,
-                            widget.color ?? Colors.white60
-                          ],
-                        ),
+    return MouseRegion(
+      onEnter: (details) {
+        setState(() {
+          mousePosition = details.position;
+        });
+      },
+      onHover: (details) {
+        setState(() {
+          mousePosition = details.position;
+        });
+      },
+      onExit: (details) {
+        setState(() {
+          mousePosition = details.position;
+        });
+      },
+      child: Stack(
+        children: [
+          if (widget.widget != null) widget.widget!,
+          if (mousePosition != Offset.zero)
+            AnimatedPositioned(
+              duration: Duration(milliseconds: (widget.speed ?? 2) * 100),
+              left: mousePosition.dx - (widget.size ?? 150 / 2),
+              top: mousePosition.dy - (widget.size ?? 150 / 2),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(180),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 3.5, sigmaY: 3.5),
+                  child: Container(
+                    width: widget.size ?? 150,
+                    height: widget.size ?? 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(180),
+                      border: Border.all(
+                        width: 3,
+                        color: Colors.grey,
+                      ),
+                      gradient: RadialGradient(
+                        colors: [
+                          Colors.transparent,
+                          widget.color ?? Colors.white60
+                        ],
                       ),
                     ),
                   ),
                 ),
               ),
-          ],
-        ),
+            ),
+        ],
       ),
     );
   }
